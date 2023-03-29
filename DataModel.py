59abc685c131e9ac064214da53fc5d4e08a9aaa2
juanyepesp <j.yepes@uniandes.edu.co>
2023-03-29 07:26:13 -0500

    Initial commit

---
+++ DataModel.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class DataModel(BaseModel):
+# Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
+    year: float
+    km_driven: float
+    seats: float
+    max_power: float
+    fuel: str
+    transmission: str
+    
+
+#Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
+    def columns(self):
+        return ["year", "km_driven", "seats", "max_power", "fuel", "transmission"]
---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from fastapi import FastAPI
+import pandas as pd
+from DataModel import DataModel
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+   return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+   return {"item_id": item_id, "q": q}
+
+
+@app.post("/predict")
+def make_predictions(dataModel: DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    model = load("assets/model.joblib")
+    result = model.predict(df)
+    return result
